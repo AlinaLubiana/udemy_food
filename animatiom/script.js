fd7b3f8f5814c315639c43613ee24bd9de3ec1f3
Alina Lubiana <alina.furman28@gmail.com>
2023-03-15 16:17:36 +0100

    add animation

---
+++ animatiom/script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('.btn');
+let timerID,
+    i = 0;
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerID); 
+//     }
+//     console.log('text'); // выводим в консоль текст
+//     i++;
+
+// }
